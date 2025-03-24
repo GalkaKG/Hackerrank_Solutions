b9ffe53cbf8e0395bd8241e2d9ff5e3942bd9074
Galina Georgieva <103485495+GalkaKG@users.noreply.github.com>
2025-03-24 21:34:27 +0200

    Update list_comprehensions.py

--- Python/Basic Data Types/list_comprehensions.py
+++ Python/Basic Data Types/list_comprehensions.py
@@ -4,9 +4,5 @@ if __name__ == '__main__':
     z = int(input())
     n = int(input())
     
-    result = [[i, j, k] for i in range(x + 1) 
-                    for j in range(y + 1) 
-                    for k in range(z + 1) 
-                    if i + j + k != n]
-                    
+    result = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != n]
     print(result)
